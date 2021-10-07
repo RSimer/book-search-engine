51f9df97463d43fb1cc6f206a0f43fc7da016010
RSimer <65471088+RSimer@users.noreply.github.com>
2021-10-07 13:38:14 -0700

    login and sign up forms fixed and complete

--- book-search-engine/client/src/utils/mutations.js
+++ book-search-engine/client/src/utils/mutations.js
@@ -1,11 +1,43 @@
 import { gql } from '@apollo/client';
 // like writing routes
-export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!) {
-    addProfile(name: $name) {
+export const LOGIN_USER = gql`
+  mutation loginUser($name: String!, $email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      _id
+      name
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
       _id
       name
       skills
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors: [String], $description: String!, $bookId: String, $image: String, $link: String, $title: String!) {
+    saveBook(bookId: $bookId, title: $title) {
+      image
+      bookId
+      title
+      description
+      
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($authors: [String], $description: String!, $bookId: String, $image: String, $link: String, $title: String!) {
+    removeBook(bookId: $bookId, title: $title) {
+       image
+      bookId
+      title
+      description
     }
   }
 `;
