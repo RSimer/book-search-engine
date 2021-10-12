ca7f0432a8c9c90bb0791ab669e4326be461fa5e
RSimer <65471088+RSimer@users.noreply.github.com>
2021-10-12 11:20:50 -0700

    working from laptop so extra debuggin is needed

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,11 +1,58 @@
 import { gql } from '@apollo/client';
 // like writing routes
-export const ADD_PROFILE = gql`
-  mutation addProfile($name: String!) {
-    addProfile(name: $name) {
+export const LOGIN_USER = gql`
+  mutation loginUser($name: String!, $email: String!, $password: String!) {
+    loginUser(email: $email, password: $password) {
+      token
+      username{_id,name}
+      
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
+      token
+      username{_id,name}
+      
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        title
+        description
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook( $bookId: ID!) {
+    removeBook(bookId: $bookId) {
       _id
       name
-      skills
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+     
     }
   }
 `;
